72a1bcd3caffd173abc7535b80c25f4652ba1d95
xafero <georg.berendt@uni-oldenburg.de>
2017-10-16 23:09:52 +0200

    Added test case

---
+++ NetEmit.Test/MinimalTests.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.IO;
+using NUnit.Framework;
+
+using static NUnit.Framework.TestContext;
+
+namespace NetEmit.Test
+{
+    [TestFixture]
+    public class MinimalTests
+    {
+        private static readonly string ResDir = Path.Combine(CurrentContext.TestDirectory, "Resources");
+
+        [Test]
+        [TestCase("FormsTest")]
+        [TestCase("NativeTest")]
+        [TestCase("ServiceTest")]
+        [TestCase("WcfTest")]
+        [TestCase("WpfTest")]
+        public void ShouldEmitSimilar(string suffix)
+        {
+            var inputAss = Path.Combine(ResDir, $"{suffix}.exe");
+            Console.WriteLine(inputAss);
+        }
+    }
+}
